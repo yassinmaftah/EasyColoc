0f351641c3e204f9ff2da32187aa58d179b3d66f
yassinmftah <yassinmaftah06@gmail.com>
2026-02-24 14:24:30 +0000

    Defined mass assignable fields and cast created_by to integer

---
+++ app/Models/Colocation.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Colocation extends Model
+{
+    protected $fillable = [
+        'name',
+        'status',
+        'created_by',
+    ];
+
+    protected function casts(): array{return ['created_by' => 'integer',];}
+}
