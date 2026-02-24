97648a82606b7831731438f3cac868b425bb0728
yassinmftah <yassinmaftah06@gmail.com>
2026-02-24 14:23:40 +0000

    Added fillable properties for new custom fields and updated casts for booleans and integers

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'reputation',
+        'is_global_admin',
+        'is_banned',
     ];
 
     /**
@@ -43,6 +46,9 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'is_global_admin' => 'boolean',
+            'is_banned' => 'boolean',
+            'reputation' => 'integer',
         ];
     }
 }
