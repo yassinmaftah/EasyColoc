38cdef92380166a99e7ab50f47925b883d2ad00f
yassinmftah <yassinmaftah06@gmail.com>
2026-02-24 14:26:33 +0000

    Defined fillable fields for tracking costs and cast the amount to a 2-decimal format

---
+++ app/Models/Expense.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Expense extends Model
+{
+    protected $fillable = [
+        'colocation_id',
+        'payer_id',
+        'category_id',
+        'title',
+        'amount',
+        'expense_date',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'colocation_id' => 'integer',
+            'payer_id' => 'integer',
+            'category_id' => 'integer',
+            'amount' => 'decimal:2',
+            'expense_date' => 'date',
+        ];
+    }
+}
