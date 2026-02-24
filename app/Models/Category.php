16d567f14f6eb09464ed50d98e8cac5f0bfd9294
yassinmftah <yassinmaftah06@gmail.com>
2026-02-24 14:26:13 +0000

    Set up fillable properties for expense categories and cast colocation_id

---
+++ app/Models/Category.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $fillable = [
+        'name',
+        'colocation_id',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'colocation_id' => 'integer',
+        ];
+    }
+}
